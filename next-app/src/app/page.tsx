085249f376ae0560fd9bb41fdfafa8e5c6904c13
Damodar Pulla <151498795+ismarthawkback@users.noreply.github.com>
2025-03-04 17:49:54 +0000

    Complete adding tailwind and chakra integration

--- next-app/src/app/page.tsx
+++ next-app/src/app/page.tsx
@@ -1,8 +1,10 @@
+import { ColorModeButton } from "@/components/ui/color-mode";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+      <ColorModeButton />
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
         <Image
           className="dark:invert"
@@ -31,7 +33,7 @@ export default function Home() {
             rel="noopener noreferrer"
           >
             <Image
-              className="dark:invert"
+              className="invert dark:invert-0"
               src="/vercel.svg"
               alt="Vercel logomark"
               width={20}
